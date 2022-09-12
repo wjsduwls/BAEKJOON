3778a3cbb8ba8e93adde067fcb2b2194fa80c561
wjsduwls <64457259+wjsduwls@users.noreply.github.com>
2022-09-13 01:25:55 +0900

    [Gold V] Title: 공약수, Time: 2260 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2436. 공약수/공약수.py
@@ -0,0 +1,20 @@
+def gcd(a,b):
+    while b:
+        temp = b
+        b = a%b
+        a = temp
+    return a
+
+g,l = map(int, input().split())
+div = l//g
+a,b = 1, div
+for i in range(1, div//2+1):
+    if div%i == 0:
+        c = i
+        d = div//i
+        if gcd(c,d) != 1:
+            continue
+        if a+b > c+d:
+            a = c
+            b = d
+print(a*g, b*g)
